69a69902e03f59fb6a09cfd50d9c0ea080b26c23
Adam Chen Wei <awei@pebblepost.com>
2018-01-09 16:06:07 -0500

    chore(): can't exclude dev stuff

--- modularization/app/src/components/App/index.js
+++ modularization/app/src/components/App/index.js
@@ -1,4 +1,5 @@
 
 import React from 'react';
+import Button from 'material-ui/Button';
 
-export default () => <h1>Hello World!!!</h1>;
+export default () => <h1>Hello World!!! <Button> ClickHere </Button></h1>;
